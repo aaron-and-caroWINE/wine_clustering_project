adbbc0d7b48625a412031d87e3cff6eea0657f37
Aaron Moore <aaron.mv.moore@gmail.com>
2023-04-18 14:37:16 -0500

    Added comments

--- explore.py
+++ explore.py
@@ -1,3 +1,4 @@
+# imports
 import pandas as pd
 import wrangle_wine as w
 
@@ -5,12 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def quality_dis(train):
 
+def quality_dis(train):
+    '''
+    Gets barplot of wine qualities
+    '''
+    
+    # establish colors
     colors = ['firebrick', 'peachpuff']
+    
+    # set pallette colors
     sns.set_palette(sns.color_palette(colors))
-    sns.histplot(train, x='quality', hue='white_wine', multiple='stack', bins=4, discrete=True)
+    
+    # create a histogram
+    sns.histplot(train, x='quality', hue='white_wine', multiple='stack',
+                 bins=4, discrete=True)
+    
+    # set the legend
     plt.legend(labels = ['White Wines','Red Wines'])
+    
+    # set the title
     plt.title('Distribution of Wine Quality')
     plt.xlabel('Wine Qualities')
     plt.ylabel('Wine Count')
@@ -18,37 +33,75 @@ def quality_dis(train):
 
     
 def volatile_acidity_vis(train):
+    '''
+    Gets boxplotqith quality and volatile acidity
+    '''
+    # set the target
     target = 'quality'
-
-    sns.boxplot(data = train, x = train[target], y = train['volatile_acidity'], palette='rocket')
+    
+    # create a boxplot with train dataset
+    sns.boxplot(data = train, x = train[target], y = train['volatile_acidity'], 
+                palette='rocket')
+    
+    # set title and labels
     plt.title('Volatile Acidity vs Quality')
     plt.xlabel('Quality')
     plt.ylabel('Volatile Acidity')
+    
+    # show the plot
     plt.show()
 
 def alcohol_vis(train):
+    '''
+    Get a box plot visualization with quality and alcohol content
+    '''
+    # set the target
     target = 'quality'
 
-    sns.boxplot(data = train, x = train[target], y = train['alcohol'], palette='rocket')
+    # create a box plot with the train dataset
+    sns.boxplot(data = train, x = train[target], y = train['alcohol'], 
+                palette='rocket')
+    
+    # set the titles and labels
     plt.title('Alcohol vs Quality')
     plt.xlabel('Quality')
     plt.ylabel('Alcohol')
+    
+    # show the visual
     plt.show()
 
 def density_vis(train):
+    '''
+    Get a box plot visual of wine density and wine quality
+    '''
+    # set target
     target = 'quality'
 
+    # create boxplot with the train dataset
     sns.boxplot(data = train, x = train[target], y = train['density'], palette='rocket')
+    
+    # set the title and the lables
     plt.title('Density vs Quality')
     plt.xlabel('Quality')
     plt.ylabel('Density')
+    
+    # show the visualization
     plt.show()
 
 def chlorides_vis(train):
+    '''
+    Creates a boxplot visualization using wine quality and chlorides
+    '''
+    # set the target
     target = 'quality'
-
+    
+    # create boxplot with the train dataset
     sns.boxplot(data = train, x = train[target], y = train['chlorides'], palette='rocket')
+    
+    # set the title and the lables 
     plt.title('Chlorides vs Quality')
     plt.xlabel('Quality')
     plt.ylabel('Chlorides')
+    
+    # show the visuals
     plt.show()
